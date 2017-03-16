b28614652e104a34509a71bd6bce4cc7959393f4
Nick Alexander <nick@appkabob.com>
2017-03-15 20:34:59 -0500

    Added Course Evaluation Report and made it and the Application Dissemination Report subclasses of Report to help with easier consistency in headers and footers

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from models.moodle_utils import Moodle
 from models.course import Course
-from resources.report import Report
+from resources.report import ApplicationDisseminationReport, CourseEvaluationReport
 from models.user import User
 
 # users = User.get_users_for_course(4)
@@ -12,6 +12,8 @@ for user in users:
     interactions = course.fetch_interactions()
     if interactions and user.userid != 6:
         print(user)
-        report = Report(course, user)
-        report.save_pdf()
+        ce_report = CourseEvaluationReport(course, user)
+        ce_report.save_pdf()
+        ad_report = ApplicationDisseminationReport(course, user)
+        ad_report.save_pdf()
         # break
--- resources/report.py
+++ resources/report.py
@@ -11,14 +11,142 @@ class Report:
     def __init__(self, course, user):
         self.course = course
         self.user = user
+        self.name = None
 
     def __repr__(self):
-        return "<Report >"
+        return "<Report {} {}>".format(self.course.name, self.user.email)
+
+    def _generate_report_content(self):
+        pass
 
     def save_pdf(self):
-        doc = SimpleDocTemplate("output/AD_BeyondCompliance_{}{}_{}.pdf".format(self.user.firstname, self.user.lastname, self.user.iein), pagesize=letter,
-                                rightMargin=36,leftMargin=36,
-                                topMargin=55,bottomMargin=40)
+        doc = SimpleDocTemplate(
+            "output/{}".format(self.name),
+            pagesize=letter,
+            rightMargin=36, leftMargin=36,
+            topMargin=55, bottomMargin=40)
+        Story = self._generate_report_content()
+        doc.build(Story, onFirstPage=self._header_footer, onLaterPages=self._header_footer)
+
+    def save_multiple_as_pdf(self):
+        pass
+
+    def _header_footer(self, canvas, doc):
+        # Save the state of our canvas so we can draw on it
+        canvas.saveState()
+        styles = getSampleStyleSheet()
+
+        # Header
+        header = Paragraph('Participant: {} {}, IEIN: {}'.format(self.user.firstname, self.user.lastname, self.user.iein), styles['Normal'])
+        w, h = header.wrap(doc.width, doc.topMargin)
+        header.drawOn(canvas, doc.leftMargin, doc.height + doc.topMargin - h)
+
+        # Footer
+        footer = Paragraph('© {} Consortium for Education Change. All rights reserved.'.format(date.today().year), styles['Normal'])
+        w, h = footer.wrap(doc.width, doc.bottomMargin)
+        footer.drawOn(canvas, doc.leftMargin, h + 16)
+
+        # Release the canvas
+        canvas.restoreState()
+
+    @staticmethod
+    def _interpret_likert(likert):
+        qa_list = []
+        i = 1
+        for qa in likert.split('___'):
+            qa_cleaned = qa.replace('__', ', ').replace('_', ' ')
+            if i % 2 == 0: # even, i.e. answer
+                new_qa.append(qa_cleaned)
+                qa_list.append(new_qa)
+            else: # odd, i.e. question
+                new_qa = [qa_cleaned]
+            i += 1
+        # print(qa_list)
+        return qa_list
+
+    @staticmethod
+    def _format_qa_pairs_for_table_display(qa_pairs):
+        styles = getSampleStyleSheet()
+        new_qa_pairs = []
+        for qa_pair in qa_pairs:
+            new_qa_pairs.append([Paragraph('<strong>{}</strong>'.format(qa_pair[0]), styles["Normal"]), Paragraph(qa_pair[1], styles["Normal"])])
+        return new_qa_pairs
+
+
+class CourseEvaluationReport(Report):
+    def __init__(self, course, user):
+        Report.__init__(self, course, user)
+        self.name = "CourseEval_BeyondCompliance_{}{}_{}.pdf".format(self.user.firstname, self.user.lastname, self.user.iein)
+
+    def __repr__(self):
+        return "<CourseEvalReport {} {}>".format(self.course.name, self.user.email)
+
+    def _generate_report_content(self):
+
+        Story = []
+        styles = getSampleStyleSheet()
+        styles.add(ParagraphStyle(name='Justify', alignment=TA_JUSTIFY))
+
+        logo = "static/img/BeyondComplianceAdminAcademyStorylineLogoHighRes.gif"
+        titletext = "COURSE EVALUATION"
+        likert_description_text = "Please select the option that <underline>best</underline> describes your experience with this course.<br /><br /><strong>Answer Choices:</strong> Disagree, Somewhat Disagree, Agree, Strongly Agree"
+
+        im = Image(logo, 2 * inch, 0.84 * inch)
+
+        title = '<font size=12>%s</font>' % titletext
+
+        questions = []
+        for interaction in sorted(self.course.interactions, key=lambda x: x.id if x.id else 'interactions_1000'):
+            if interaction.slide_id == 'Scene1_Slide15_LikertScale_0_0':
+                questions.extend(self._interpret_likert(interaction.student_response))
+
+            if interaction.slide_id == 'Scene1_Slide16_LikertScale_0_0':
+                questions.extend(self._interpret_likert(interaction.student_response))
+
+            if interaction.slide_id == 'Scene1_Slide17_Essay_0_0':
+                essay_response = interaction.student_response
+
+        # print(questions)
+        questions = self._format_qa_pairs_for_table_display(questions)
+
+        t = Table(questions, [3.6 * inch])
+        t.setStyle(TableStyle([('VALIGN', (0, 0), (-1, -1), 'TOP'),
+                               ('INNERGRID', (0, 0), (-1, -1), 0.25, colors.black),
+                               ('BOX', (0, 0), (-1, -1), 0.25, colors.black),
+                               ]))
+
+        # PRINT OUT REPORT
+
+        Story.append(im)
+
+        Story.append(Spacer(1, 12))
+
+        Story.append(Paragraph(title, styles["Title"]))
+        Story.append(Paragraph(likert_description_text, styles["Normal"]))
+
+        Story.append(Spacer(1, 12))
+
+        Story.append(t)
+
+        Story.append(Paragraph('How could this academy be improved?', styles["Heading2"]))
+        Story.append(Paragraph(essay_response, styles["Normal"]))
+
+        return Story
+        # doc.build(Story, onFirstPage=self._header_footer, onLaterPages=self._header_footer)
+
+
+class ApplicationDisseminationReport(Report):
+    def __init__(self, course, user):
+        Report.__init__(self, course, user)
+        self.name = "AD_BeyondCompliance_{}{}_{}.pdf".format(self.user.firstname, self.user.lastname, self.user.iein)
+
+    def __repr__(self):
+        return "<AppDissReport {} {}>".format(self.course.name, self.user.email)
+
+    def _generate_report_content(self):
+        # doc = SimpleDocTemplate("output/{}".format(self.name), pagesize=letter,
+        #                         rightMargin=36, leftMargin=36,
+        #                         topMargin=55, bottomMargin=40)
 
         Story = []
         styles = getSampleStyleSheet()
@@ -133,7 +261,7 @@ class Report:
         # Story.append(Paragraph(part2btitle, styles["Heading2"]))
         # Story.append(Paragraph(part2bbody, styles["Normal"]))
 
-        # Story.append(Spacer(1, 12))
+
 
         progress_date_answer = None
         for interaction in self.course.interactions:
@@ -163,22 +291,5 @@ class Report:
 
         Story.append(KeepTogether(part2b))
 
-        doc.build(Story, onFirstPage=self._header_footer, onLaterPages=self._header_footer)
-
-    def _header_footer(self, canvas, doc):
-        # Save the state of our canvas so we can draw on it
-        canvas.saveState()
-        styles = getSampleStyleSheet()
-
-        # Header
-        header = Paragraph('Participant: {} {}, IEIN: {}'.format(self.user.firstname, self.user.lastname, self.user.iein), styles['Normal'])
-        w, h = header.wrap(doc.width, doc.topMargin)
-        header.drawOn(canvas, doc.leftMargin, doc.height + doc.topMargin - h)
-
-        # Footer
-        footer = Paragraph('© {} Consortium for Education Change. All rights reserved.'.format(date.today().year), styles['Normal'])
-        w, h = footer.wrap(doc.width, doc.bottomMargin)
-        footer.drawOn(canvas, doc.leftMargin, h + 16)
-
-        # Release the canvas
-        canvas.restoreState()
+        return Story
+        # doc.build(Story, onFirstPage=self._header_footer, onLaterPages=self._header_footer)
